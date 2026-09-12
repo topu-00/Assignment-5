c6e8e33d74543d3df4dedf19d598447b6d81b5d1
Topu Sarker <topusorker087@gmai.com>
2026-09-12 18:36:53 +0600

    Push1: setup project structure

---
+++ DevStack-App/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">
+        Dev Stack
+      </h1>
+      <button className="btn btn-primary">
+        Test DaisyUI
+      </button>
+    </div>
+  )
+}
+
+export default App
